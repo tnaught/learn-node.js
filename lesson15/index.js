d0219d1757136c60e83edf9d87b581800c309165
tianqin <tianqin@dianxinos.com>
2015-01-27 19:16:01 +0800

    add lesson15

---
+++ lesson15/index.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+var Cat = mongoose.model('Cat', {
+    name: String,
+    friends: [String],
+    age: Number
+});
+var kitty = new Cat({name: 'kitty', friends: ['tom', 'jerry']});
+kitty.age = 3;
+
+kitty.save(function(err) {
+    if(err) {
+        console.log(err);
+    }
+});
